2931788d62f6391d7bd09a56f699f5c44c83ac60
Hafiz Muhammad Owais <owaisilyas543@gmail.com>
2025-06-25 15:08:54 +0500

    React Class 1 Boiler Plate

---
+++ MyReactApp/src/main.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div style={{
+    backgroundColor: "#282c34", // Dark Charcoal
+    color: "#61dafb", // React Blue
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "2.5rem",
+    fontFamily: "Segoe UI, sans-serif"
+  }}>
+    Hello World & Hello React !!
+  </div>
+);
+
+
